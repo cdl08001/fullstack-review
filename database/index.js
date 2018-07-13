a1c0ea81f910a91d0baa2aa4332ee4054fd5811a
Calin Lewis <calin.lewis@gmail.com>
2018-07-13 13:01:45 -0700

    Update repoSchema

--- database/index.js
+++ database/index.js
@@ -2,15 +2,26 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  name: String,
+  full_name: String,
+  owner: {
+    login: String,
+    html_url: String
+  }
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = () => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
 }
+
+  fluffy.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    fluffy.speak();
+  });
 
 module.exports.save = save;
